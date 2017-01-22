0d55243f5a8a07f2d9dfa36ce670b5174ee593e1
Corey Mason <corey.mason98@googlemail.com>
2017-01-21 19:49:18 -0800

    Switch Authvars to global

--- js/authvar.js
+++ js/authvar.js
@@ -2,12 +2,12 @@ initApp = function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var uid = user.uid;
-      var providerData = user.providerData;
+      /*var */displayName = user.displayName;
+      /*var */email = user.email;
+      /*var */emailVerified = user.emailVerified;
+      /*var */photoURL = user.photoURL;
+      /*var */uid = user.uid;
+      /*var */providerData = user.providerData;
       /*user.getToken().then(function(accessToken) {
         document.getElementById('sign-in-status').textContent = 'Signed in';
         document.getElementById('sign-in').textContent = 'Sign out';
@@ -23,9 +23,9 @@ initApp = function() {
       });*/
     } else {
       // User is signed out.
-      document.getElementById('sign-in-status').textContent = 'Signed out';
-      document.getElementById('sign-in').textContent = 'Sign in';
-      document.getElementById('account-details').textContent = 'null';
+      //document.getElementById('sign-in-status').textContent = 'Signed out';
+      //document.getElementById('sign-in').textContent = 'Sign in';
+      //document.getElementById('account-details').textContent = 'null';
     }
   }, function(error) {
     console.log(error);
