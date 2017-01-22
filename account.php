0fc660b099783a4c252ac320751cc24e3457ecf5
Neeraj Aggarwal <aggarwalneeraj141@users.noreply.github.com>
2017-01-21 22:56:44 -0800

    style profile page

--- account.php
+++ account.php
@@ -2,27 +2,39 @@
 <head>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+<body>
 <div class="sup">
-<table width="100%">
-	<tr>
-		<td width="33.33%">
-			<center>
-			<a href="create_project.php"><div class="button shadow">NEW PROJECT</div></a>
-			</center>
-		</td>
-		<td width="33.33%">
-			<center>
-			<a href="main_page.php"><div class="button shadow">HOME</div></a>
-			</center>
-		</td>
-		<td  width="33.33%">
-			<center>
-			<a href="account.php"><div class="button shadow">MY ACCOUNT</div></a>
-			</center>
-		</td>
-	</TR>
-</TABLE>
+			<table width="100%">
+				<tr>
+					<td width="33.33%">
+						<center>
+						<a href="create_project.php" style="color: #000;"><div class="button shadow">NEW PROJECT</div></a>
+						</center>
+					</td>
+					<td width="33.33%">
+						<center>
+						<a href="main_page.php" style="color: #000;"><div class="button shadow">HOME</div></a>
+						</center>
+					</td>
+					<td  width="33.33%">
+						<center>
+						<a href="account.php" style="color: #000;"><div class="button shadow">MY ACCOUNT</div></a>
+						</center>
+					</td>
+				</tr>
+			</table>
+		</div>
+
+<div style="width:80%; margin: 0 auto; padding-top: 1em;">
+	<img class="shadow" id="profile_pic" src="/profile.jpg" width="300" height="300">
+<br>
+	<div style="padding-top: 2em;">
+		<h2>Name : <span><?php echo $username; ?></span></h2><br>
+		<h2 style="margin-top: -1em;">email: <span><?php echo $row['email']?></span></h2><br>
+		<p style="font-size: .8em; font-weight: normal; margin-top: -1em;">this is where the bio will be</p>
+	</div>
 </div>
+
 <?php
  $uid = $_COOKIE['uid'];
  include('db.php');
@@ -31,14 +43,7 @@
  $username = $row['username'];
 ?>
 
-<div>
-<center>
-<img class="shadow" id="profile_pic" src="<?php echo $row['photourl']; ?>" width="200" height="200">
-</center>
-Nome : <span><?php echo $username; ?></span><br>
-email: <span><?php echo $row['email']?></span><br>
-</div>
-<br>
+
 
 <?php
 
@@ -47,7 +52,7 @@ include('db.php');
 $mydata = $mysqli->query("SELECT * FROM `project` WHERE username = '".$username."' ORDER BY datetime DESC;");
 $num_row = $mydata->num_rows;
 
-?>
+?> 
 
 <?php
 
@@ -66,5 +71,5 @@ for($i = 0; $i < $num_row; $i++){
 }
 
 ?>
-
+</body>
 </html>
