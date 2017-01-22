78cfb7ec3cc1ed2e367c88398eda5108bbda3e83
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-01-22 01:23:05 -0800

    nav bar changes

--- account.php
+++ account.php
@@ -1,29 +1,33 @@
 <html>
 <head>
+
 <link rel="stylesheet" type="text/css" href="style.css">
+
+ <script src="https://www.gstatic.com/firebasejs/3.6.6/firebase.js"></script>
+    <script src="creds/creds.js"></script>
+    <script>firebase.initializeApp(config);</script>
+    <script src="https://cdn.firebase.com/libs/firebaseui/1.0.0/firebaseui.js"></script>
+    <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/1.0.0/firebaseui.css" />
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="js/custom.js"></script>
+    <link rel="stylesheet" href="css/main.css"/>
+
+    <script src="js/authvar.js"></script>
+
 </head>
 <body>
 <div class="sup">
-			<table width="100%">
-				<tr>
-					<td width="33.33%">
-						<center>
-						<a href="create_project.php" style="color: #000;"><div class="button shadow">NEW PROJECT</div></a>
-						</center>
-					</td>
-					<td width="33.33%">
-						<center>
-						<a href="main_page.php" style="color: #000;"><div class="button shadow">HOME</div></a>
-						</center>
-					</td>
-					<td  width="33.33%">
-						<center>
-						<a href="account.php" style="color: #000;"><div class="button shadow">MY ACCOUNT</div></a>
-						</center>
-					</td>
-				</tr>
-			</table>
-		</div>
+<ul>
+	<li><a href="main_page.php" style="color: #FFF;">Explore</a></li>
+	<li><a href="create_project.php" style="color: #FFF;">Create</a></li>
+	<li><a href="account.php" style="color: #FFF;">Profile</a></li>
+</ul>
+</div>
 
 <div style="width:80%; margin: 0 auto; padding-top: 1em;">
 	<img class="shadow" id="profile_pic" src="/profile.jpg" width="300" height="300">
